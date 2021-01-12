c5f1fbd6996f9fd128a738e2755892a99ff2e141
guilher47 <guilher47@gmail.com>
2021-01-12 13:27:27 -0300

    :package: form orcamento

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
+    document.querySelector(".sidebar").classList.toggle("show-menu")
 );
